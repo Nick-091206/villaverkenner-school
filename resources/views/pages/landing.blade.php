233d1389e8fac1976c94ba934b0750f3fd8b70d9
Nick Colijn <nickcolijn2006@gmail.com>
2025-04-09 21:36:54 +0200

    files added

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,12 @@
+<x-layout title="Home">
+  <main class="home-main">
+    <div class="text-section">
+      <h1 class="title">Villa Verkenner</h1>
+      <p class="subtitle">Ontdek je droom vakanite villa</p>
+    </div>
+    <div class="search">
+      <input type="text" placeholder="Zoek in Oostenrijk...">
+      <button title="Search"><i class="fa-solid fa-location-dot"></i></button>
+    </div>
+  </main>
+</x-layout>
